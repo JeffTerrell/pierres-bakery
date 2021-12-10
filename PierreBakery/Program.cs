caeb0621960fdcd6f56f5dc97bade784d7b9eab6
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-10 18:04:04 -0500

    WIP: working on UI console logic(sum sub total to total)

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -9,8 +9,8 @@ class Program
     Console.WriteLine("Welcome to Pierre's Bakery!");
     Console.WriteLine("Bread = $5 ------ Pastries = $2");
     Console.WriteLine("Today's specials:");
-    Console.WriteLine("- Bread: buy 2 loaves of bread get 1 Free");
-    Console.WriteLine("- Pastries: 3 for $5, 4 for $7, 5 for $9 or 6 for $10");
+    Console.WriteLine("* Bread: buy 2 loaves of bread get 1 free!");
+    Console.WriteLine("* Pastries: 3 for $5, 4 for $7, 5 for $9 or 6 for $10!");
     Console.WriteLine("Would you like to order bread? (yes/no)");
     string userBread = Console.ReadLine().ToLower();
     if (userBread == "yes")
@@ -19,8 +19,43 @@ class Program
       int userBreadAmount = int.Parse(Console.ReadLine());
       Bread userBreadOrder = new Bread(userBreadAmount);
       Console.WriteLine($"{userBreadAmount} loaves of bread added"); 
-      Console.WriteLine($"Sub total: {userBreadOrder.GetBreadTotal()}");
-    }
+      Console.WriteLine($"Sub total: ${userBreadOrder.GetBreadTotal()}");
+
+      Console.WriteLine("Would you like to order pastries? (yes/no)");
+        string userPastry = Console.ReadLine().ToLower();
+        if (userPastry == "yes")
+        {
+          Console.WriteLine("How many pastries? (max: 6)");
+          int userPastryAmount = int.Parse(Console.ReadLine());
+          Pastry userPastryOrder = new Pastry(userPastryAmount);
+          Console.WriteLine($"{userPastryAmount} pastries added");
+          Console.WriteLine($"Sub total: ${userPastryOrder.GetPastryTotal()}");
+          Console.WriteLine($"Order total: ${userBreadOrder.GetBreadTotal() + userPastryOrder.GetPastryTotal()}");
+          Console.WriteLine("Return to 'main menu' or 'exit'? (main/exit)");
+          string userEnd = Console.ReadLine().ToLower();
+          if (userEnd == "main")
+            {
+              Main();
+            }
+              else
+              {
+                Environment.Exit(0);
+              }
+        }
+          else
+          {
+            Console.WriteLine("Return to 'main menu' or 'exit'? (main/exit)");
+            string userEnd = Console.ReadLine().ToLower();
+            if (userEnd == "main")
+            {
+              Main();
+            }
+              else
+              {
+                Environment.Exit(0);
+              }
+          }
+    }      
       else
       {
         Console.WriteLine("Would you like to order pastries? (yes/no)");
