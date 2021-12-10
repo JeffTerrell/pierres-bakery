84c32d8ac75699a936e1f05ea97ce2a290957e7c
Jeff Terrell <jeffaterrell@gmail.com>
2021-12-10 16:46:54 -0500

    add failing logic for Pastry-test7(5 pastries)

--- PierreBakery.Tests/ModelTests/PastryTest.cs
+++ PierreBakery.Tests/ModelTests/PastryTest.cs
@@ -91,5 +91,20 @@ namespace PierreBakery.Tests
       //Assert
       Assert.AreEqual(expectedAmount, orderAmount);
     }
+
+    [TestMethod]
+    public void GetPastryTotal_ReturnValueOfTotalPastryOrderFor5Pastries_Int()
+    {
+      //Arrange
+      int testAmount = 5;
+      Pastry newOrder = new Pastry(testAmount);
+      int expectedAmount = 9;
+
+      //Act
+      int orderAmount = newOrder.GetPastryTotal();
+
+      //Assert
+      Assert.AreEqual(expectedAmount, orderAmount);
+    }
   }
 }
